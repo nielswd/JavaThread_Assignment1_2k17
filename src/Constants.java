a7b40c4ad69bc09c03d80c43861f898bf7ad54da
NielsWD <niels.waughray-dupoux@epitech.eu>
2017-03-21 11:30:22 +0000

    2nd commit

--- src/Constants.java
+++ src/Constants.java
@@ -12,11 +12,14 @@ public class Constants {
     public static final int MAX_VEHICULES_HOLDING = 1000;
     public static final int TOTAL_VEHICULES = 2000;
 
+    public static final int NUMBER_OF_STUDENT = 1500;
+    public static final int NUMBER_OF_TEACHER = 500;
+
     public static final int MAX_ENTRANCE_EXIT = 3;
     public enum ENTRANCE_EXIT {
         ENTRANCE_EXIT_1,
         ENTRANCE_EXIT_2,
-        ENTRANCE_EXIT_3;
+        ENTRANCE_EXIT_3
     }
 
     public static final double VEHICULE_MAX_SPACE_TAKEN = 1.5;
@@ -25,6 +28,6 @@ public class Constants {
 
     public enum CAR_STATE {
         PARKED,
-        NOT_PARKED;
+        NOT_PARKED
     }
 }
---
+++ src/ParkManagement.java
@@ -0,0 +1,6 @@
+/**
+ * Created by iNfecteD on 21/03/2017.
+ */
+public class ParkManagement {
+
+}
